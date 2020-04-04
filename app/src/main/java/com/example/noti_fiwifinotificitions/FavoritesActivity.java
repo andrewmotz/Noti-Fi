006bc877fa07aa0d2b8ae3e549aeeb13f7ab494e
Andrew Motz <andrewmozza555@gmail.com>
2020-04-04 14:37:17 -0700

    Fixed issue with being able to add "Select a Network:" to saved network list. Spinners now update properly.

--- app/src/main/java/com/example/noti_fiwifinotificitions/FavoritesActivity.java
+++ app/src/main/java/com/example/noti_fiwifinotificitions/FavoritesActivity.java
@@ -93,6 +93,7 @@ public class FavoritesActivity extends AppCompatActivity {
         FavSSIDS favSSIDS = new FavSSIDS(getSharedPreferences(MainActivity.NOTI_FI_PREF, MODE_PRIVATE));
         ArrayList<String> favList = favSSIDS.getFavSSIDList();
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, favList);
+        spinnerAdapter.setDropDownViewResource(R.layout.my_spinner);
         favSpinner.setAdapter(spinnerAdapter);
 
         //Fill Listview
