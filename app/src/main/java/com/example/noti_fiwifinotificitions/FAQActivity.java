bd277514f50524d0b35168548f6ca2e0870c1029
Elysia Stella <56214196+TriggerHappyEly@users.noreply.github.com>
2020-04-04 16:57:30 -0700

    Made textview scrollable in FAQ layout

--- app/src/main/java/com/example/noti_fiwifinotificitions/FAQActivity.java
+++ app/src/main/java/com/example/noti_fiwifinotificitions/FAQActivity.java
@@ -3,12 +3,21 @@ package com.example.noti_fiwifinotificitions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class FAQActivity extends AppCompatActivity {
+
+    TextView faqText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+
+        faqText = findViewById(R.id.faq_text);
+
+        //to make the textview scrollable
+        faqText.setMovementMethod(new ScrollingMovementMethod());
     }
 }
