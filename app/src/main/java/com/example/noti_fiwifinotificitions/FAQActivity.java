84bda82ac61d5820dfeaf3be8af5780d4057a2f2
Elysia Stella <56214196+TriggerHappyEly@users.noreply.github.com>
2020-04-03 20:56:00 -0700

    Added 3 new activities

---
+++ app/src/main/java/com/example/noti_fiwifinotificitions/FAQActivity.java
@@ -0,0 +1,14 @@
+package com.example.noti_fiwifinotificitions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FAQActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
+}
