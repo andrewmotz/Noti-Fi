9b5a3e53012f3dc7346adcf945d9ab25a597c47a
Andrew Motz <andrewmozza555@gmail.com>
2020-04-03 18:39:21 -0700

    Revert "- Added "Custom Network" activity for manually entering a network"
    This reverts commit 59ab44e4bec4f8ba3b71c3c827a9abdb350fc3f5.

--- app/src/main/java/com/example/noti_fiwifinotificitions/AddNotificationActivity.java
+++ app/src/main/java/com/example/noti_fiwifinotificitions/AddNotificationActivity.java
@@ -9,7 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,7 +16,6 @@ public class AddNotificationActivity extends AppCompatActivity {
 
     Spinner networkSpinner;
     EditText contentText;
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +24,6 @@ public class AddNotificationActivity extends AppCompatActivity {
         contentText = findViewById(R.id.ContentEditText);
 
         networkSpinner = findViewById(R.id.spinner_networks);
-        textView = findViewById(R.id.show_custom_ssid);
 
         //Fill spinner
         FavSSIDS favSSIDS = new FavSSIDS(getSharedPreferences(MainActivity.NOTI_FI_PREF, MODE_PRIVATE));
@@ -38,17 +35,7 @@ public class AddNotificationActivity extends AppCompatActivity {
     public void saveButton(View view){
         SavedNetworks savedNetworks = new SavedNetworks(getSharedPreferences(MainActivity.NOTI_FI_PREF, MODE_PRIVATE));
         String content = contentText.getText().toString();
-
-        String ssid;
-
-        //if ssid not manually entered, ssid is from spinner
-        if (textView.getText().toString().equals("Empty")) { //make sure this matches the strings.xml
-            ssid = networkSpinner.getSelectedItem().toString();
-        }
-        else { //if ssid was manually entered, extract ssid from below the spinner
-            ssid = textView.getText().toString();
-        }
-
+        String ssid = networkSpinner.getSelectedItem().toString();
         savedNetworks.addNotiFi(ssid, content);
 
         goToMainActivity();
@@ -63,13 +50,5 @@ public class AddNotificationActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
-    protected void onResume() { //to receive the string if manually entered
-        super.onResume();
 
-        Intent intent = getIntent();
-        String ssid = intent.getStringExtra(CustomNetwork.SAVED_CUSTOM_SSID);
-        textView.setText(ssid);
-
-    }
 }
