519bb252e83a046087dfa342990313faae903aa5
Elysia Stella <56214196+TriggerHappyEly@users.noreply.github.com>
2020-04-04 19:46:19 -0700

    Moved tutorials over to Help activity instead of FAQ activity

--- app/src/main/java/com/example/noti_fiwifinotificitions/HelpActivity.java
+++ app/src/main/java/com/example/noti_fiwifinotificitions/HelpActivity.java
@@ -3,12 +3,21 @@ package com.example.noti_fiwifinotificitions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
+
+    TextView helpText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        helpText = findViewById(R.id.help_text);
+
+        //to make the textview scrollable
+        helpText.setMovementMethod(new ScrollingMovementMethod());
     }
 }
