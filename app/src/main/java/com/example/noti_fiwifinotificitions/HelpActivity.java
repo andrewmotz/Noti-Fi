e705f68850229bec01daffecd23fda96a2960dda
Andrew Motz <andrewmozza555@gmail.com>
2020-04-04 20:37:12 -0700

    Added custom menu to help activity

--- app/src/main/java/com/example/noti_fiwifinotificitions/HelpActivity.java
+++ app/src/main/java/com/example/noti_fiwifinotificitions/HelpActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -26,7 +28,24 @@ public class HelpActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.help_menu, menu);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Help");
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.permissions_vid:
+                showVideo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showVideo(){
+
     }
 }
